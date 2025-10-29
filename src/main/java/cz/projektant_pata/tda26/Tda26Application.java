56b52dad053b3c21fd7845a562ead30a40d6d792
projektant-pata <hyvlri22@zaci.spse.cz>
2025-10-29 11:38:06 +0100

    ahoj

---
+++ src/main/java/cz/projektant_pata/tda26/Tda26Application.java
@@ -0,0 +1,13 @@
+package cz.projektant_pata.tda26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tda26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tda26Application.class, args);
+	}
+
+}
