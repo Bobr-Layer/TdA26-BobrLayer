d28d420d6cd547e46b92b08d7f6befea6b99d900
projektant-pata <hyvlri22@zaci.spse.cz>
2025-12-05 09:10:42 +0100

    bez user

--- src/main/java/cz/projektant_pata/tda26/model/course/Course.java
+++ src/main/java/cz/projektant_pata/tda26/model/course/Course.java
@@ -1,6 +1,7 @@
 package cz.projektant_pata.tda26.model.course;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import cz.projektant_pata.tda26.model.course.quiz.Quiz;
 import cz.projektant_pata.tda26.model.user.User;
 import cz.projektant_pata.tda26.model.course.material.Material;
 import jakarta.persistence.*;
@@ -32,6 +33,13 @@ public class Course {
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Material> materials = new ArrayList<>();
 
+//    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz> quizzes = new ArrayList<>();
+
+    private List<Quiz> feed = new ArrayList<>();
+
+
+
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private Instant createdAt;
@@ -44,7 +52,6 @@ public class Course {
     @JoinColumn(name = "lector_id", nullable = false)
     private User lector;
 
-    // V souboru Course.java přidejte:
 
     @ManyToMany
     @JoinTable(
