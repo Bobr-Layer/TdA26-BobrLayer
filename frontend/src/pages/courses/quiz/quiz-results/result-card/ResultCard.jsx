e988bcf24e657254247b0538193bfc0a623bc1c6
petr.machovec <petr.machovec@rppi.cz>
2026-01-14 17:40:58 +0100

    smazani dosavadniho frontendu

--- frontend/src/pages/courses/quiz/quiz-results/result-card/ResultCard.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './result-card.module.scss';
-
-export default function ResultCard({number, correct}) {
-  return (
-    <p className={`${styles.result_card} ${!correct ? styles.incorrect : ''}`}>
-      {number}
-    </p>
-  )
-}
--- frontend/src/services/Api.jsx
+++
@@ -1,3 +0,0 @@
-const Api = 'http://localhost:8080/api';
-
-export default Api;
